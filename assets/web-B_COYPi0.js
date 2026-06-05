d830c68ba61e079d530dc7c38a39c5a047490294
azatilfakovich1993-coder <azatilfakovich1993-coder@users.noreply.github.com>
2026-06-05 05:50:19 +0000

    deploy: 4facd80a4e16e41913e3c0aa57dc9926d67cd284

--- assets/web-B_COYPi0.js
+++ assets/web-B_COYPi0.js
@@ -1,4 +1,4 @@
-import{W as gt}from"./index-DQxJ6shg.js";var ge={};/**
+import{W as gt}from"./index-2fVU5p2r.js";var ge={};/**
  * @license
  * Copyright 2017 Google LLC
  *
--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/nur-hayat/sw.js', { scope: '/nur-hayat/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
