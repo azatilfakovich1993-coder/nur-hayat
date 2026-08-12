9171598a1462d1c9501b55ca92a66d28d317118d
azatilfakovich1993-coder <azatilfakovich1993-coder@users.noreply.github.com>
2026-08-12 10:57:21 +0000

    deploy: bab2a12db311fa1017d4146d45ca252981b17892

---
+++ assets/nextAuthRoute-BWtGvLYj.js
@@ -0,0 +1,1 @@
+function e(t){if(t)return"/home";try{if(localStorage.getItem("onboarding_seen")==="1")return"/auth"}catch{}return"/onboarding"}export{e as n};
--- assets/web-EbXTThwy.js
+++ assets/web-EbXTThwy.js
@@ -1,4 +1,4 @@
-import{W as gt}from"./index-ChVkiMRs.js";var ge={};/**
+import{W as gt}from"./index-Lo3BvoqM.js";var ge={};/**
  * @license
  * Copyright 2017 Google LLC
  *
